bb182619c738a763946b42c8429c7d7717531157
BJ Nguyen <nguyenxbj@gmail.com>
2018-08-22 20:10:59 -0700

    Add structure. Add problem one.

---
+++ EulerSolutions/EulerSolutions/Solutions/Problem-1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EulerSolutions.Solutions
+{
+	class Problem_1
+	{
+		//Console.WriteLine("Problem 1 Solution!");
+	}
+}
